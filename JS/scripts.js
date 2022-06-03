53e807ecf25fb7a180bc20ef53b59eaf5444510b
Emmanuelnz <emmanuelnun75@gmail.com>
2022-06-03 04:36:38 -0400

    added code for functionality
    mainly for the buttons and making the list work with inputs

---
+++ JS/scripts.js
@@ -0,0 +1,34 @@
+function newItem() {
+
+// Adding a new item to list of items
+  let li = $('<li></li>');
+  let inputValue = $('input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  }
+    else {
+      $('#list').append(li);
+    }
+
+// Crossing out an item from list of items
+  function crossout() {
+    li.toggleClass('strike')
+  }
+
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+// Adding Delete Button "X"
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+      crossOutButton.on('click',deleteListItem);
+      function deleteListItem() {
+        li.addClass('delete')
+      }
+}
+// Reordering items
+  $('list').sortable();
